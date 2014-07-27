57626be2ac4677fa05a80517c5140bb209676646
aedonis <dr.tomm@seznam.cz>
2014-07-27 12:49:43 +0200

    validation on comments added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  validates :commenter, presence: true,
+		length: {minimum: 3}
+  validates :comment, presence: true,
+		length: {minimum: 10}
 end
