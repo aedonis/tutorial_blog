f1c73786cc14429a25be703ec759ef20cb8efb16
aedonis <dr.tomm@seznam.cz>
2014-07-27 12:56:38 +0200

    removed validation on messages for now, wasn't working anyway

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
-  validates :commenter, presence: true,
-		length: {minimum: 3}
-  validates :comment, presence: true,
-		length: {minimum: 10}
+
 end
